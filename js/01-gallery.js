6dd22b6babb50140fe088f8ced614c6c719335fe
Loki99911 <nikita_kresik@i.ua>
2022-10-22 18:41:33 +0300

    add Start Files

---
+++ js/01-gallery.js
@@ -0,0 +1,57 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galeryEl = document.querySelector(".gallery");
+
+function createGaleryItems(items) {
+  return items
+    .map(
+      (item) => `
+<div class="gallery__item">
+  <a class="gallery__link" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</div>`
+    )
+    .join("\n");
+}
+
+galeryEl.insertAdjacentHTML("beforeend", createGaleryItems(galleryItems));
+
+const onModalCall = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  openModal(event.target.dataset.source);
+};
+
+galeryEl.addEventListener("click", onModalCall);
+
+function openModal(sourse) {
+  const instance = basicLightbox.create(`
+    <img src=${sourse} width="800" height="600">
+`);
+
+  instance.show();
+
+  document.addEventListener("keydown", (event) => {
+    console.dir(event.code);
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+// НЕ ЗНАЮ КАК СНЯТЬ :((((
+  //   document.removeEventListener("keydown", (event) => {
+  //     console.dir(event.code);
+  //     if (event.code === "Escape") {
+  //       instance.close();
+  //     }
+  //   });
+}
+
+// console.log(galleryItems);
